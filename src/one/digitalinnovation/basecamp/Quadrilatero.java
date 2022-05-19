fd830b77f36f08bdb88aaae6f41d29c730aa5c2f
CWebler <acweblersantos@gmail.com>
2022-05-19 15:08:28 -0300

    Aula-02

--- src/one/digitalinnovation/basecamp/Quadrilatero.java
+++ src/one/digitalinnovation/basecamp/Quadrilatero.java
@@ -3,9 +3,11 @@ package one.digitalinnovation.basecamp;
 public class Quadrilatero {
 
     public static void area(double lado){
+
         System.out.println("Área do quadrado é: " + lado * lado);
     }
     public static void area(double lado1, double lado2){
+
         System.out.println("Área do retângulo é: " + lado1 * lado2);
     }
     public static void area(double baseMaior, double baseMenor, double altura){

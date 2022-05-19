c4fecc2d223bed4604f904abb0ac999f6240c153
CWebler <acweblersantos@gmail.com>
2022-05-19 15:00:28 -0300

    Aula-02

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício Quadrilátero");
+        Quadrilatero.area(6);
+        Quadrilatero.area(7d,7d);
+        Quadrilatero.area(9,6,3);
+        Quadrilatero.area(7f,7f);
+    }
+}
